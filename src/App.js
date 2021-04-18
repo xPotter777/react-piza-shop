c03b60a2f5e5104a3a54739c5cafb47a8cce07fb
Alexandr Senkiv <senkivsasa@gmail.com>
2021-04-18 21:43:11 +0300

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+
+import { Header } from './components';
+import { Home, Cart } from './pages';
+import { Route } from 'react-router-dom';
+
+function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3001/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
